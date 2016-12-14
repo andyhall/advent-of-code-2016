b329947f00ab1c40b258475dedb18e2e23d8597c
andyhall <andy@fenomas.com>
2016-12-14 15:56:12 +0900

    Cleaned up 09.

--- src/09.js
+++ src/09.js
@@ -1,51 +1,32 @@
 'use strict'
 /* globals log, part1, part2 */
 
+
 module.exports = function (txt) {
-
-    // txt = 'ADVENT'
-    // txt = '(3x3)XYZ'
-    // txt = 'A(2x2)BCD(2x2)EFG'
-    // txt = '(6x1)(1x3)A'
-    // txt = 'X(8x2)(3x3)ABCY'
-
-    return solve(txt)
+    part1(solve(txt, 1))
+    part2(solve(txt, 2))
 }
 
 
-
-function solve(txt) {
-
-    var s = ''
-    var dat = txt.split('\n')
-    for (var line of dat) s += line
-
-
-    var doingPart2
+function solve(txt, part) {
+    var s = txt.split('\n').join('')
 
     function count(s) {
         var p1 = s.indexOf('(', 0)
         if (p1 < 0) return s.length
-        var arr = /^\((\d+)x(\d+)\)/.exec(s.substr(p1))
-        if (!arr) { console.warn('??'); return 0 }
-        var plen = arr[0].length
-        var a = parseInt(arr[1])
-        var b = parseInt(arr[2])
-        var body = s.substr(p1 + plen, a)
+        var result = /^\((\d+)x(\d+)\)/.exec(s.substr(p1))
+        var plen = result[0].length
+        var a = parseInt(result[1])
+        var b = parseInt(result[2])
         var right = s.substr(p1 + plen + a)
-        if (doingPart2) return p1 + b * count(body) + count(right)
-        return p1 + a * b + count(right)
+        if (part === 1) return p1 + a * b + count(right)
+        var body = s.substr(p1 + plen, a)
+        return p1 + b * count(body) + count(right)
     }
 
-    
-    part1(count(s))
-
-    doingPart2 = true
-    part2(count(s))
-
-
-
+    return count(s)
 }
+
 
 
 
